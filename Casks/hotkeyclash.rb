b85354c47d29f6982319263e72918bd5ab19f93b
Wunderlandmedia <104343552+Wunderlandmedia@users.noreply.github.com>
2026-09-24 08:58:57 +0200

    hotkeyclash: drop deprecated verified parameter
    Fixes #1

--- Casks/hotkeyclash.rb
+++ Casks/hotkeyclash.rb
@@ -2,8 +2,7 @@ cask "hotkeyclash" do
   version "0.9.0"
   sha256 "d6f3b326414d7abd310cfca86d472563a30463f779d8e96e9ec48502aa8d7c59"
 
-  url "https://github.com/Wunderlandmedia/HotkeyClash/releases/download/v#{version}/HotkeyClash-#{version}.dmg",
-      verified: "github.com/Wunderlandmedia/HotkeyClash/"
+  url "https://github.com/Wunderlandmedia/HotkeyClash/releases/download/v#{version}/HotkeyClash-#{version}.dmg"
   name "HotkeyClash"
   desc "Menu bar app that detects keyboard shortcut conflicts"
   homepage "https://hotkeyclash.com/"
